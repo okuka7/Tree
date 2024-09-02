591ecbda30f5589a69120b6bf6e1f7e8ec91557e
taguyoon <qorghab123@gmail.com>
2024-09-02 18:56:28 +0900

    fix

--- src/main/java/kr/or/iei/user/controller/UserController.java
+++ src/main/java/kr/or/iei/user/controller/UserController.java
@@ -1,5 +1,20 @@
 package kr.or.iei.user.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.iei.user.model.service.UserService;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(value = "/joinFrm")
+	public String joinFrm() {
+		return "user/joinFrm";
+	}
 }
--- src/main/java/kr/or/iei/user/model/service/UserService.java
+++ src/main/java/kr/or/iei/user/model/service/UserService.java
@@ -1,5 +1,8 @@
 package kr.or.iei.user.model.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
 }

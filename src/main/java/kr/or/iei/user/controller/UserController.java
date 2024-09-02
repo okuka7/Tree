6904f572297b0d5d386354cd60b4355d9248a72b
taguyoon <qorghab123@gmail.com>
2024-09-02 19:25:50 +0900

    login

--- src/main/java/kr/or/iei/user/controller/UserController.java
+++ src/main/java/kr/or/iei/user/controller/UserController.java
@@ -3,8 +3,11 @@ package kr.or.iei.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+import kr.or.iei.user.model.dto.UserDto;
 import kr.or.iei.user.model.service.UserService;
 @RequestMapping(value="/user")
 public class UserController {
@@ -14,5 +17,14 @@ public class UserController {
 	@GetMapping(value = "/joinFrm")
 	public String joinFrm() {
 		return "user/joinFrm";
+	}
+	@PostMapping(value = "/login")
+	public String login(UserDto u, HttpSession session) {
+		System.out.println(u);
+//		UserDto user = userService.selectOneUser(u);	
+//		if(user != null) {
+//			session.setAttribute("user", user);
+//		}
+		return "redirect:/";
 	}
 }

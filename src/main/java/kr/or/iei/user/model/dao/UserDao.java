dd7e5b3d8c4e815a2d40cac7e093e1f9d7543588
soobean02 <gnh09092@gmail.com>
2024-09-02 19:06:34 +0900

    Dao

--- src/main/java/kr/or/iei/user/model/dao/UserDao.java
+++ src/main/java/kr/or/iei/user/model/dao/UserDao.java
@@ -3,6 +3,6 @@ package kr.or.iei.user.model.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class UserDao {
+public interface UserDao {
 
 }
--- src/main/java/kr/or/iei/user/model/dto/UserDto.java
+++ src/main/java/kr/or/iei/user/model/dto/UserDto.java
@@ -1,5 +1,20 @@
 package kr.or.iei.user.model.dto;
 
-public class UserDto {
+import org.apache.ibatis.type.Alias;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="user")
+public class UserDto {
+	private int userNo;
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userEmail;
+	private String enrollDate;
 }

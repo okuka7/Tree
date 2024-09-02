e82ad6fe5861833c53222647c25465c1cd379071
taguyoon <qorghab123@gmail.com>
2024-09-02 19:07:10 +0900

    setting2

--- src/main/java/kr/or/iei/user/model/dto/UserDto.java
+++ src/main/java/kr/or/iei/user/model/dto/UserDto.java
@@ -1,5 +1,15 @@
 package kr.or.iei.user.model.dto;
 
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="user")
 public class UserDto {
 
 }

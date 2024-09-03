a8e75ba23038ff7b872c075c0cb09c6483898eb9
gusxo <gusxo004@naver.com>
2024-09-03 18:46:07 +0900

    fixed

--- src/main/java/kr/or/iei/calendar/controller/CalendarController.java
+++ src/main/java/kr/or/iei/calendar/controller/CalendarController.java
@@ -2,6 +2,7 @@ package kr.or.iei.calendar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.iei.calendar.model.service.CalendarService;
@@ -13,6 +14,9 @@ public class CalendarController {
     @Autowired
     private CalendarService calendarService;
 
-
+    @GetMapping(value="/list")
+    public String list(){
+        return "calendar/calendar";
+    }
 
 }

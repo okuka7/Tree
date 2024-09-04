1bf84457499a6ddb61b3e4efce00b10999f2f5a5
gusxo <gusxo004@naver.com>
2024-09-04 19:58:03 +0900

    calendar

--- src/main/java/kr/or/iei/calendar/model/dao/CalendarDao.java
+++ src/main/java/kr/or/iei/calendar/model/dao/CalendarDao.java
@@ -3,8 +3,11 @@ package kr.or.iei.calendar.model.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.or.iei.calendar.model.dto.Calendar;
+import kr.or.iei.user.model.dto.UserDto;
 
 @Mapper
 public interface CalendarDao {
 	int insertCalender(Calendar calendar);
+
+    Calendar selectCalendar(UserDto user);
 }
--- src/main/java/kr/or/iei/calendar/model/service/CalendarService.java
+++ src/main/java/kr/or/iei/calendar/model/service/CalendarService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import kr.or.iei.calendar.model.dao.CalendarDao;
 import kr.or.iei.calendar.model.dto.Calendar;
+import kr.or.iei.user.model.dto.UserDto;
 
 @Service
 public class CalendarService {
@@ -16,7 +17,9 @@ public class CalendarService {
 		int result = calendarDao.insertCalender(calendar);
 		return result;
 	}
-    public Calendar getInfo() {
-        Calendar calendar = calendarDao.getInfo();
+    public Calendar getInfo(UserDto user) {
+        Calendar calendar = calendarDao.selectCalendar(user);
+        System.out.println(calendar);
+        return calendar;
     }
 }
